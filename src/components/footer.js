5fc16a4b1c5e9ca2f77b26d42f77435b6b4d5ba6
Jorge S. Sanchez <georotzen@gmail.com>
2023-04-19 19:22:44 -0700

    Modified footer in footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import './footer.css';
+import logoUCLA from '../components/pages/media/ucla_engineering_logo_300.jpg'
 
 function Footer() {
     return (
         <div className='footerContainer'>
             <div className='footerInfo'>
                 <div className='InfoItem'>
-                    <h1>Engineer Change.</h1>
-                    <p>UCLA Samueli School of Engineering 
+                    <p><img src = {logoUCLA} alt ='UCLA'/></p>
+                    <p>
                         Department of Civil and Environmental Engineering
                         Los Angeles, CA 90095</p>
                 </div>
                 <div className='InfoItem'>
-                    <h1> Contact </h1>
+                    <h1> Main Contact: </h1>
                     <p>Sriram Narasimhan <br></br>Email: snarasim@ucla.edu</p>
                 </div>
             </div>
 
         <div className='footerBar'>
             <div className='website-rights'>
-                © 2022 SRI Lab
+            <b>© 2023 Sensing &amp; Robotics for Infrastructure Lab</b>
             </div>
         </div>    
         </div>
